2ad3e937ba863981f4c567f0afbf9d4b9ae417aa
root <root@Jordan-PC.localdomain>
2020-12-16 16:14:56 -0500

    Integrated Clarifai API for face detection in user provided images

--- src/App.js
+++ src/App.js
@@ -3,9 +3,15 @@ import Navigation from './components/Navigation/Navigation.js';
 import Logo from './components/Logo/Logo.js';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.js';
 import Rank from './components/Rank/Rank.js';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition.js';
 import './App.css';
 import 'tachyons';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
+
+const app = new Clarifai.App({
+  apiKey: 'd3f84857b0694d2e88500ec8772bafce'
+ });
 
 const particlesOptions = {
   particles: {
@@ -29,13 +35,40 @@ const particlesOptions = {
 };
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: ''
+    }
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+  onButtonSubmit = (event) => {
+    this.setState({imageUrl: this.state.input});
+    app.models
+      .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+      .then(
+        function(response) {
+          console.log(response);
+        },
+        function(err) {
+          // An error occured
+        }
+      );
+  }
+
   render() {
     return (
       <div className="App">
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
+        <FaceRecognition imageUrl={this.state.imageUrl}/>
         <Particles className='particles' params={particlesOptions}/>
       </div>
     );
